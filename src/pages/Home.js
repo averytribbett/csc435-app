3b0f2daba4cb1e768c214b45628d2556bd3e4ad4
Avery Tribbett <apatribbett03@gmail.com>
2024-01-20 17:41:39 -0600

    Added heading

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import "./../styles/home.css";
 export const Home = () => {
   return (
     <div class="home-div">
+      <h1>Coming Soon! CSP Store</h1>
       <p>Avery Tribbett</p>
       <p>CSC435 - Adv Web App Dev</p>
       <p>Assignment 2</p>
