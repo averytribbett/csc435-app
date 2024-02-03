484d4a725193201bcb66d898c05382e2e44f4ade
Avery Tribbett <apatribbett03@gmail.com>
2024-02-02 20:00:04 -0600

    Add account and cart icons to navbar, also set up cart and account pages

---
+++ src/pages/Account.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { NavBar } from "../components/Nav/NavBar";
+
+
+export const Account = () => {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  )
+}
---
+++ src/pages/Cart.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { NavBar } from "../components/Nav/NavBar";
+
+
+export const Cart = () => {
+  return (
+    <div>
+      <NavBar/>
+
+    </div>
+  )
+}
