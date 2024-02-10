931c7d6f29e8a983e187b33c448d2d655ccffe19
Avery Tribbett <apatribbett03@gmail.com>
2024-02-10 13:46:44 -0600

    Implemented user login redux store and logout on account page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./../styles/home.css";
 import { ProductContainer } from "../components/Products/ProductContainer";
 import { NavBar } from "../components/Nav/NavBar";
+import { useSelector } from "react-redux";
+import { selectUser } from "../stores/userSlice";
+import { useNavigate } from "react-router-dom";
 
 export const Home = () => {
+  const user = useSelector(selectUser);
+  const navigate = useNavigate();
   const [search, setSearch] = useState("");
+
+  // user is not logged in navigate to login
+  useEffect(() => {
+    if (!user) navigate("/")
+  }, []);
 
   return (
     <div className="home-div">
@@ -12,11 +22,14 @@ export const Home = () => {
         onSearch={setSearch} // Extract search from Nav
       />
       <div className="info-div">
-        <h1>Coming Soon! CSP Store Example Products Below Click Add To Cart!</h1>
+        <h1>Coming Soon! CSP Store</h1>
+        {user ? (
+          <h2>{`Email: ${user.email}`}</h2>
+        ): null}
         <p>Avery Tribbett</p>
         <p>CSC435 - Adv Web App Dev</p>
-        <p>Assignment 4</p>
-        <p>02/04/2024</p>
+        <p>Assignment 5</p>
+        <p>02/11/2024</p>
         <p>Icons by <a target="_blank" href="https://icons8.com">Icons8</a></p>
       </div>
       <ProductContainer
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,24 +1,74 @@
 import React, { useState } from "react";
 import "./../styles/login.css";
-import { Link } from "react-router-dom";
 import { UserProfileForm } from "../components/UserProfileForm";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { login } from "../stores/userSlice";
 
 
 export const Login = () => {
   const [loginState, setLoginState] = useState(true);
+  // Form data and handlers
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleSubmit = () => {
+    // Call login dispath function when the user logs in
+    if (loginState) {
+      dispatch(login({
+        email: email,
+        password: password,
+      }));
+      navigate("/home");
+    } else {
+      // @TODO link user to database for signup
+      // NOTE after user signs up they will need to login with their account
+    }
+  }
 
   return (
     <div className="login-box">
       <h1 className="login-heading">{loginState ? "Log In" : "Create Account"}</h1>
       <form className="login-form">
-        <input className="input-box" type="email" placeholder="Email" required/>
+        <input 
+          className="input-box" 
+          type="email" 
+          placeholder="Email" 
+          required
+          onChange={(e) => setEmail(e.target.value)}
+        />
         {!loginState && // If in sign up state add UserProfileForm
-          <UserProfileForm/>
+          <UserProfileForm
+            setUsername={setUsername}
+            setFirstName={setFirstName} 
+            setLastName={setLastName} 
+          />
         }
-        <input className="input-box" type="password" placeholder="Password" required/>
-        {/*Temporarily links to home page will enventually be submit button for the form*/}
-        <Link className="submit-btn form-btn" to="/home">{loginState ? "Log In" : "Create Account"}</Link>
-        <input className="toggle-state-btn form-btn" type="button" value={loginState ? "Or Create Account" : "Or Log In"} onClick={() => setLoginState(!loginState)}/>
+        <input 
+          className="input-box" 
+          type="password" 
+          placeholder="Password" 
+          required
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input 
+          className="submit-btn form-btn" 
+          value={loginState ? "Log In" : "Create Account"}
+          type="button"
+          onClick={handleSubmit}
+        />
+        <input 
+          className="toggle-state-btn form-btn" 
+          type="button" 
+          value={loginState ? "Or Create Account" : "Or Log In"} 
+          onClick={() => setLoginState(!loginState)}
+        />
       </form>
     </div>
   )
